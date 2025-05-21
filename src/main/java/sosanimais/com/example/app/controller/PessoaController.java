576a9f563c84b1a5ef069ba238f3d3b4d6ee8c8d
Lucas Galindo <srlucasmtg@gmail.com>
2025-05-21 17:57:31 -0300

    fix: atualizacao paginas de pessoas e criacao de listagem geral

--- src/main/java/sosanimais/com/example/app/controller/PessoaController.java
+++ src/main/java/sosanimais/com/example/app/controller/PessoaController.java
@@ -60,13 +60,13 @@ public class PessoaController {
     }
 
 
-//    @GetMapping("/buscaid/{id}")
-//    public ResponseEntity<Object> getPessoaId(@PathVariable Long id){
-//        Pessoa aux = pessoaService.getId(id);
-//        if(aux!=null)
-//            return ResponseEntity.ok(aux);
-//        return ResponseEntity.badRequest().body(new Erro("Erro ao achar Pessoa"));
-//    }
+    @GetMapping("/busca-id/{id}")
+    public ResponseEntity<Object> getPessoaId(@PathVariable Long id){
+        Pessoa aux = pessoaService.getId(id);
+        if(aux!=null)
+            return ResponseEntity.ok(aux);
+        return ResponseEntity.badRequest().body(new Erro("Erro ao achar Pessoa"));
+    }
 
     @GetMapping("/busca-cpf/{cpf}")
     public ResponseEntity<Object> getPessoaCPF(@PathVariable String cpf){
